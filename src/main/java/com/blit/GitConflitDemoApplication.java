2be718df91dc136f845cd31437b3ceb9f8e442c0
David <pschji@gmail.com>
2023-03-10 20:42:41 -0500

    add alphabet for demo

---
+++ src/main/java/com/blit/GitConflitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.blit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConflitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitConflitDemoApplication.class, args);
+	}
+
+}
